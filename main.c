fce546f10930a18632de7465db13a4bfe6c1a9ab
nikgora <nik.gora.2004@gmail.com>
2022-03-04 20:24:47 +0200

    2

--- main.c
+++ main.c
@@ -6,11 +6,15 @@
 #define N 100
 
 void print (char str[]);
+char * reverse (char str[]);
 
 int main ()
 {
     char str[N];
+    int i =0,j=0;
     fgets(str,N,stdin);
+    print(reverse(str));
+    print("\n");
     print(str);
     return 0;
 }
@@ -18,4 +22,11 @@ int main ()
 void print (char str[])
 {
     printf("%s",str);
+}
+char * reverse (char str[]){
+    char  res [strlen(str)+1];
+    for (int i = 0; i < strlen(str); ++i) {
+        res[i]=str[strlen(str)-i-1];
+    }
+    return res;
 }
