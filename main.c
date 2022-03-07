74d4621e51072085fa397087efd3f9b809fb2e77
nikgora <nik.gora.2004@gmail.com>
2022-03-07 15:17:51 +0100

    reverse ready

--- main.c
+++ main.c
@@ -5,39 +5,44 @@
 
 #define N 100
 
-void print (char str[]);
-char * reverse (char str[]);
+void print(char str[]);
 
-int main ()
-{
-    char str[N]="13";
-    int i =0,j=0;
-    fgets(str,N,stdin);
-    print(str);
-    print("\n");
-    print(reverse(str));
-    print("\n");
+void print_reverse(char str[]);
 
-    print(str);
+char *reverse(char str[]);
+
+char *scanword();
+
+int main() {
+    char str[N];
+    char symbol;
+    int i = 0, j = 0;
+    do {
+        scanf("%c", &symbol);
+        printf("%c", symbol);
+    } while (symbol != '.');
     return 0;
 }
 
-void print (char str[])
-{
-    printf("%s",str);
+void print(char str[]) {
+    printf("%s", str);
 }
-char * reverse (char str[]){
-    char  res [N]="";
-    int j=0;
+
+char *reverse(char str[]) {
+    char res[N] = "";
+    int j = 0;
     for (int i = 0; i < strlen(str); i++) {
-        if(str[strlen(str)-i-1]!='\0'){
-            res[j]=str[strlen(str)-i-1];
+        if (str[strlen(str) - i - 1] != '\0') {
+            res[j] = str[strlen(str) - i - 1];
             j++;
         }
     }
     for (int i = 0; i < strlen(str); ++i) {
-        printf("i=%d res[i]=%c\n",i,res[i]);
+        printf("i=%d res[i]=%c\n", i, res[i]);
     }
     return res;
+}
 
+void print_reverse(char str[]) {
+    printf("%s", reverse(str));
 }
