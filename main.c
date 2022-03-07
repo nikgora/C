b253ddc8a8e7d356290d874b36202ca10904221a
nikgora <nik.gora.2004@gmail.com>
2022-03-07 14:37:56 +0100

    reverse ready

--- main.c
+++ main.c
@@ -10,11 +10,14 @@ char * reverse (char str[]);
 
 int main ()
 {
-    char str[N];
+    char str[N]="13";
     int i =0,j=0;
     fgets(str,N,stdin);
+    print(str);
+    print("\n");
     print(reverse(str));
     print("\n");
+
     print(str);
     return 0;
 }
@@ -24,16 +27,17 @@ void print (char str[])
     printf("%s",str);
 }
 char * reverse (char str[]){
-    char  res [strlen(str)];
+    char  res [N]="";
     int j=0;
-    for (int i = 0; i < strlen(str); ++i) {
-        res[i]='\0';
-    }
-    for (int i = 0; i < strlen(str); ++i) {
+    for (int i = 0; i < strlen(str); i++) {
         if(str[strlen(str)-i-1]!='\0'){
-            j++;
             res[j]=str[strlen(str)-i-1];
+            j++;
         }
     }
+    for (int i = 0; i < strlen(str); ++i) {
+        printf("i=%d res[i]=%c\n",i,res[i]);
+    }
     return res;
+
 }
