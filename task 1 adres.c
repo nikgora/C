34acce215dcdbc386c94277332c37064e778d83b
nikgora <nik.gora.2004@gmail.com>
2022-03-08 10:09:14 +0100

    first task ready

--- task 1 adres.c
+++ task 1 adres.c
@@ -1,1 +1,59 @@
 #include "task 1 adres.h"
+
+void print_adres(char *str[]) {
+    for (int i = 0; i < strlen(*str); ++i) {
+        printf("%c", *str[i]);
+    }
+}
+
+char *reverse_adres(char *str[]) {
+    char res[N] = "";
+    int j = 0;
+    for (int i = 0; i < strlen(*str); i++) {
+        if (*str[strlen(*str) - i - 1] != '\0') {
+            res[j] = *str[strlen(*str) - i - 1];
+            j++;
+        }
+    }
+    /*
+    for (int i = 0; i < strlen(str); ++i) {
+        printf("i=%d res[i]=%c\n", i, res[i]);
+    }
+    */
+    return res;
+}
+
+void print_reverse_adres(char *str[]) {
+    print_adres(reverse_adres(str));
+}
+
+char *scanword_adres(char *word[]) {
+    char symbol;
+    int i = 0;
+    do {
+        scanf("%c", &symbol);
+        *word[i] = symbol;
+        i++;
+    } while (symbol != ' ' && symbol != '.');
+    return word;
+}
+
+char *skipspace_adres(char *word[]) {
+    char res[N] = "";
+    _Bool q = 0;
+    int j = 0;
+    for (int i = 0; i < strlen(*word); ++i) {
+        if (*word[i] == ' ' && q) {
+            res[j] = *word[i];
+            j++;
+            q = 0;
+        }
+        if (*word[i] != ' ') {
+            res[j] = *word[i];
+            q = 1;
+            j++;
+        }
+    }
+    if (res[strlen(res) - 1] == ' ')res[strlen(res) - 1] = '\0';
+    return res;
+}
--- task 1 adres.h
+++ task 1 adres.h
@@ -2,3 +2,23 @@
 #define C_AIN_TASK_1_ADRES_H
 
 #endif //C_AIN_TASK_1_ADRES_H
+
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 100
+
+
+void print_adres(char *str[]);//вывод строки в прямом порядке принимает строку
+
+void print_reverse_adres(char *str[]);//вывод строки в обратном порядке принимает строку
+
+char *reverse_adres(char *str[]);//инвертирование строки принимает строку результат строка
+
+char *scanword_adres(char *word[]);//ввод строки  принимает строку в которую надо записать строку результат строка
+
+char *skipspace_adres(
+        char *word[]);//пропуск серии пробелов принимает строку которую надо отфарматировать результат отфаромотированая строка
+
